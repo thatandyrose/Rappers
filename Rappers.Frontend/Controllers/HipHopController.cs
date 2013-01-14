23978a5a515791bccc5cfd28530a13de0b560f0f
Andy <thatandyrose@gmail.com>
2013-01-14 23:06:08 +1100

    Started adding a frontend for the Rappers

---
+++ Rappers.Frontend/Controllers/HipHopController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rappers.HipHop.Services;
+using Rappers.HipHop.Services.Implementations.S3;
+
+namespace Rappers.Frontend.Controllers
+{
+    public class HipHopController : Controller
+    {
+        private readonly IParseLogs _logParser;
+        public HipHopController()
+        {
+            _logParser = new S3LogParser();
+        }
+
+        public ActionResult S3Logs()
+        {
+           // _logParser.
+            return View();
+        }
+        
+    }
+}
