163fd47997c96a6cc13049611f7d4acf5447f566
Andy <thatandyrose@gmail.com>
2013-02-15 17:51:12 +1100

    new datetime extension

---
+++ Rappers.Baseline/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rappers.Baseline.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static double ToUnixTime(this DateTime source)
+        {
+            return source.ToUniversalTime()
+               .Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))
+               .TotalMilliseconds;
+        }
+    }
+}
